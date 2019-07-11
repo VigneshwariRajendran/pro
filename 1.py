d5fa46f35066d52f066ea27d7f992fd5d84a384d
VigneshwariRajendran <51949214+VigneshwariRajendran@users.noreply.github.com>
2019-07-11 21:54:28 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,12 @@
+qw=int(input())
+ad=[]
+for i in range(0,qw):
+  io=input()
+  ad.append(io)
+ff=[]
+for i in zip(*ad):
+  if(i.count(i[0]==len(i)):
+    ff.append(i[0])
+  else:
+    break
+ print(''.join(ff))   
