f983acc70c8b15d645fb7b9d144f97c14f93240e
VigneshwariRajendran <51949214+VigneshwariRajendran@users.noreply.github.com>
2019-07-11 22:47:20 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,11 @@
+aa,bb=map(str,input().split())
+x=0
+if len(aa)>len(bb):
+   aa,bb=bb,aa
+s=0
+while s<len(aa):
+   x+=(ord(bb[s])-ord(aa[s]))
+   p+=1
+for s in range(s,len(bb)):
+   x+=ord(bb[s])-ord('a')+1
+print(y)
